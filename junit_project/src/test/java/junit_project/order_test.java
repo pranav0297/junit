0e4bebbb53875b161cc449956d8e2f7f3ed7c3d4
pranav0297 <90312227+pranav0297@users.noreply.github.com>
2021-10-02 23:21:27 +0530

    first commit

--- junit_project/src/test/java/junit_project/order_test.java
+++ junit_project/src/test/java/junit_project/order_test.java
@@ -4,158 +4,86 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class order_test {
 	
 	
 	@Test
-	public void should_call_default_Contructor() {
-		
-		
-		//given
-		
-		//when 
-	     Order o = new Order();
-		
-		
-		//then
-		equals(o);
-
-}
-
-	@Test
 	public void should_call_Perameter_Contructor_whenThree_valuePass() {
-		
 		
 		//given
 		 int quantity=2;
 	     String itemName="test";
 	     double price=200;
 		
-		
-		//when 
+	// when 
 	     Order o = new Order(quantity,itemName,price);
 		
-		
-		//then
-		equals(o);
+//then
+	    		 assertEquals(itemName,o.getItemName());
+	    		 assertEquals(quantity,o.getQuantity());
+	    		assertNotSame(price,o.getPrice());
 }
 	
 	@Test
-	public void should_return_Quantity_when_getQuantity() {
-		 Order o = new Order();
+	public void should_getQuantity_when_setQuantity() {
+		 
 		
 		//given
-		
+		Order o = new Order();
 		
 		//when 
-	     int n = o.getQuantity();
+	     o.setQuantity(50);
 		
 		
 		//then
-		equals(n);
+		assertEquals(50,o.getQuantity());
+}
+	
+	
+	@Test
+	public void should_getItemName_when_setItemName() {
+		 
+		
+		//given
+		Order o = new Order();
+		
+		//when 
+	 o.setItemName("hello");
+		
+		
+		//then
+	 assertEquals("hello",o.getItemName());
+}
+	@Test
+	public void should_getPrice_when_setPrice() {
+		 
+		
+		//given
+		Order o = new Order();
+		
+		//when 
+	 o.setPrice(200d);
+		
+		
+		//then
+	 assertNotSame( 200 , o.getPrice());
 }
 	
 	@Test
-	public void should_return_this_when_setQuantity() {
-		 Order o = new Order();
+	public void should_getPriceWithTex_when_setPriceWithTex() {
+		 
 		
 		//given
-		int n=0;
+		Order o = new Order();
 		
 		//when 
-	  Order n1 = o.setQuantity(n);
+	 o.setPriceWithTex(200);
 		
 		
 		//then
-		equals(n1);
-}
-	
-	@Test
-	public void should_return_ItemName_when_getItemName() {
-		 Order o = new Order();
-		
-		//given
-		
-		//when 
-	 String s =  o.getItemName();
-		
-		
-		//then
-		equals(s);
-}
-	
-	@Test
-	public void should_return_this_when_setItemName() {
-		 Order o = new Order();
-		
-		//given
-		String s1 = "world";
-		
-		
-		//when 
-	   Order n= o.setItemName(s1);
-		
-		
-		//then
-		equals(n);
-}
-	
-	@Test
-	public void should_return_price_when_getPrice() {
-		 Order o = new Order();
-		
-		//given
-		
-		
-		//when 
-	     double n1 =  o.getPrice();
-		
-		
-		//then
-		equals(n1);
-}
-	@Test
-	public void should_return_this_when_setPrice() {
-		 Order o = new Order();
-		
-		//given
-		double n = 1;
-		
-		//when 
-	     Order n1 =  o.setPrice(n);
-		
-		
-		//then
-		equals(n1);
-}
-	
-	@Test
-	public void should_return_priceWithTex_when_getpriceWithTex() {
-		 Order o = new Order();
-		
-		//given
-		
-		//when 
-	     double n1 =  o.getPriceWithTex();
-		
-		
-		//then
-		equals(n1);
-}
-	
-	@Test
-	public void should_return_this_when_setpriceWithTex() {
-		 Order o = new Order();
-		
-		//given
-		double n = 1;
-		
-		//when 
-	     Order n1 =  o.setPriceWithTex(n);
-		
-		
-		//then
-		equals(n1);
+	 assertNotSame( 200 , o.getPriceWithTex());
 }
 	
 }
